bd17e7d8b179142d8a5ab68b89d2eb79ca28b136
subramanian.v <subramanian.v@caratlane.com>
2021-04-09 10:26:08 +0530

    Code cleanup

--- app/lib/constants.js
+++ app/lib/constants.js
@@ -1,14 +1,7 @@
-export const APP_NAME = process.env.APP_NAME || "node-boilerplate";
+export const APP_NAME = process.env.APP_NAME || "notificationservice";
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const LOGPATH = process.env.LOGPATH || "logs";
-export const LOG_FILE_NAME = `${LOGPATH}/${
-  APP_NAME +
-  new Date().getFullYear() +
-  "-" +
-  (new Date().getMonth() + 1) +
-  "-" +
-  new Date().getDate()
-}.log`;
+export const LOG_FILE_NAME = `${LOGPATH}/${APP_NAME}.log`;
 export const APP_PORT = 8080;
 export const TEST_PORT = 8081;
 export const TEST_URL = `http://127.0.0.1:${TEST_PORT}`;
